6e9e06dc9cb0f328c5d32ba56b0d7a27b9a67b2f
Celso Miranda <contacto@celsomiranda.net>
2015-02-28 01:17:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# [Sass] renderer for [Hexo] with [Bourbon] support
+
+An up-to-date [node-sass] wrapper that includes [Bourbon] mixins.
+
+## Install
+
+``` bash
+$ npm i hexo-renderer-bourbon
+```
+
+## Options
+
+
+
+## Example `_config.yml`
+
+``` yaml
+bourbon:
+  outputStyle: nested
+  precision: 8
+  imagePath: images
+  sourceComments: none
+  includePaths: 
+    - ''
+  indentedSyntax: false
+  omitSourceMapUrl: false
+  sourceMapEmbed: false
+  sourceMapContents: false
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var renderer = require('./lib/renderer');
+var assign = require('object-assign');
+
+hexo.config.bourbon = assign({
+  file: data.path,
+  imagePath: 'images' ,
+  omitSourceMapUrl: false,
+  indentedSyntax: false,
+  precision: 8,
+  outputStyle: 'nested',
+  sourceComments: 'none' ,
+  sourceMapEmbed: false,
+  sourceMapContents: false
+}, hexo.config.bourbon);
+
+
+hexo.extend.renderer.register('scss', 'css', renderer);
+hexo.extend.renderer.register('sass', 'css', renderer);
